28cd709334e91786b87b85b1e0c564c431446563
farizaamzal <amzalfariza44@gmail.com>
2025-09-26 11:29:03 +0200

    Update SecondActivity.java

--- SecondActivity.java
+++ SecondActivity.java
@@ -1,1 +1,38 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // Récupère le nom envoyé depuis MainActivity
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(MainActivity.NAME);
+
+        // Affiche "Hello [nom]"
+        TextView textView = findViewById(R.id.textHello);
+        textView.setText("Hello " + name);
+
+        // ImageView : juste pour afficher l'image
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.image); // ton image dans drawable
+
+        // Bouton Next
+        Button nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(v -> {
+                Intent thirdIntent = new Intent(this, ThirdActivity.class);
+                startActivity(thirdIntent); // lance la ThirdActivity
+        });
+    }
+}
+
 
