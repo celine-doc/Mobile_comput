0fa328cf1e80b808f32b53498a00efaa61bddea6
farizaamzal <amzalfariza44@gmail.com>
2025-09-26 11:29:25 +0200

    Update ThirdActivity.java

--- ThirdActivity.java
+++ ThirdActivity.java
@@ -1,1 +1,19 @@
 
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        // Bouton Previous pour revenir en arrière
+        Button previousButton = findViewById(R.id.previousButton);
+        previousButton.setOnClickListener(v -> finish()); // finish() ferme cette activité et revient à la précédente
+    }
+}
